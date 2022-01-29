de12048b2cb828500f2e3848e8d5cf1b35b366ce
wakaflorien <waka.florien45@gmail.com>
2022-01-29 19:39:28 +0200

    new

--- js/blog.js
+++ js/blog.js
@@ -1,24 +1,24 @@
 fetch('https://my-capstone-project-api.herokuapp.com/posts/')
-.then( (response) =>{
+.then((response) =>{
     return response.json()
 })
-.then( (data) => {
+.then((data) => {   
     let posts = data.posts
-    // console.log(posts)
+    console.log(posts)
     posts.map((post)=> {
-        let main = document.getElementById("main");
+        let main = document.getElementById("main")
         let article = `
                 <article class="post-one">
                     <figure class="img-one">
                         <p class="link"></p>
                         <h1 class="title" id="title">${post.title}</h1>
                         <h4>${post.subTitle}</h4>
-                        <img src="${post.imageUrl}" alt="First img" class="pic-one">
                         <p class="date">${post.dateCreated}</p>
+                        <img src="${post.imageUrl}" alt="First img" class="pic-one">
                     </figure>
                     <section class="post-one-p">
-                        <p id="p" class="p">${post.postBody}</p>
-                        <button class="read-more" onClick="readMore(${post._id})" id="${post._id}">Read More</button>
+                        <p id="p" class="p">${post.postBody+post._id}</p>
+                        <button class="read-more" onClick="readMore(1)" id="${post._id}">Read More</button>
                     </section>
                 </article>`;
 
@@ -26,10 +26,11 @@ fetch('https://my-capstone-project-api.herokuapp.com/posts/')
                 main.innerHTML += article
     })
 })
-.catch( (err) => {
-    console.log(err)
+.catch((err) => {
+    console.error(err)
 })
 
 const readMore = (id) => {
+    console.log(id)
     location.href = `./singlepost-one.html?id=${id}`
 }
